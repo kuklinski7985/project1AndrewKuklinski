5ca4add8b12d490581d392c7fc5cf33e16f48a73
Andrew Kuklinski <anku7985@colorado.edu>
2017-10-04 20:51:55 -0600

    creating file structure

---
+++ src/main.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <stdint.h>
+#include "platform.h"
+#include "project1.h"
+#include "memory.h"
+#include "conversion.h"
+#include "debug.h"
+#include <math.h>
+
+int main(){
+  
+ #ifdef PROJECT1
+ project1();
+ #endif
+  return 0;
+}
+
+
+
+
